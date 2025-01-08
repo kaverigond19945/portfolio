40d7306f8c10f2bf9b5dd9b02e7450cba688552e
kaverigond19945 <95864163+kaverigond19945@users.noreply.github.com>
2025-01-09 00:44:41 +0530

    Add files via upload

---
+++ Project/Bankio/js/bankio.js
@@ -0,0 +1,43 @@
+$('#caro1').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+$('#caro2').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
+$('#caro3').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
+AOS.init();
